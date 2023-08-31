697f922b1eb42768ab9bae6f35df45e8166933d9
Thomas Lelong <Thomas.Lelong@student.hogent.be>
2023-09-01 01:07:00 +0200

    Second

--- src/main/java/com/springboot/AsielBeheer/OpdrachtEwdj20222023ThomasLelong1AsielBeheerApplication.java
+++ src/main/java/com/springboot/AsielBeheer/OpdrachtEwdj20222023ThomasLelong1AsielBeheerApplication.java
@@ -2,12 +2,24 @@ package com.springboot.AsielBeheer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
-public class OpdrachtEwdj20222023ThomasLelong1AsielBeheerApplication {
+@EnableJpaRepositories("repository")
+@EntityScan("domain")
+public class OpdrachtEwdj20222023ThomasLelong1AsielBeheerApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpdrachtEwdj20222023ThomasLelong1AsielBeheerApplication.class, args);
 	}
+
+	@Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+	   registry.addRedirectViewController("/", "/guest");
+    }
 
 }
